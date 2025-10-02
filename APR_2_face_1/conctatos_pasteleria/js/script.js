2108069e7836225af5be95deb78efa6398bf5cb5
Fran503chichico <148788194+Fran503chichico@users.noreply.github.com>
2025-10-01 22:23:09 -0600

    Delete APR_2_face_1 directory

--- APR_2_face_1/conctatos_pasteleria/js/script.js
+++
@@ -1,5 +0,0 @@
-// Simulación de envío del formulario
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("¡Mensaje enviado correctamente!");
-});
